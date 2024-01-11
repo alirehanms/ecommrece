7e4f2b2e3229807b272300ceb03865d01615874d
ali <ali6777622@gmail.com>
2024-01-11 03:20:48 -0800

    ff

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,7 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Rating } from 'src/ratings/entities/rating.entity';
 import { link } from 'fs';
-
+//the commoit
 @Injectable()
 export class ProductsService {
   constructor(
